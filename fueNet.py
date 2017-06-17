4a2b02132b32b7f4e8b33b1f66e9dcabbca8e1f6
iguanaus <johnpeurifoy@yahoo.com>
2017-06-17 12:53:10 -0400

    First Commit

---
+++ fueNet.py
@@ -0,0 +1,157 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__	import print_function
+
+import numpy as np
+import argparse, os
+import tensorflow as tf
+from tensorflow.python.ops import init_ops
+from tensorflow.contrib.rnn import BasicLSTMCell, BasicRNNCell, GRUCell
+import pandas as pd
+import re
+
+word_level = False
+notstates = False
+
+#THis needs to take in the data, then return the data in a list of [weekday,seconds*1000,intPrice,volume]
+#The output should be in a np array form. Note that the y value doesn't have to be returned. 
+
+def file_data(filename):
+    filename="Data/06_01.csv"
+    myVals = pd.DataFrame.from_csv(filename)
+    #myVals['seconds'] = (myVals['seconds']*100.0).astype(int)
+    print("MY vals: " , myVals)
+    myVals['normPrice'] = myVals['intPrice'] - myVals['intPrice'].mean()
+    myVals['stdPrice'] = (myVals['normPrice'])/myVals['normPrice'].std()*1.0
+    myVals['normVolume'] = myVals['volume'] - myVals['volume'].mean()
+    myVals['stdVolume'] = (myVals['normVolume'])/myVals['normVolume'].std()*1.0
+    standardDev = myVals['normPrice'].std()*1.0
+    meanVal = myVals['intPrice'].mean()
+    print ("standardDev: " , standardDev)
+    print ("Mean val: " , meanVal)
+
+    
+
+    newmyVals = myVals[['stdPrice','stdVolume']]
+    allData = newmyVals.as_matrix().astype(float)
+    return allData , standardDev , meanVal
+
+
+def main():
+	# --- Set data params ----------------
+	#Create Data
+	max_len_data = 1000000000
+
+	data , standardDev,meanVal= file_data(None)
+
+	n_input = len(data[0])
+
+	n_output = 1
+	n_hidden = 40
+	learning_rate = 0.001
+	decay = 0.9
+	numEpochs = 10
+
+	#Structure of this will be [weekday,seconds*1000,intPrice,volume]
+
+	X = tf.placeholder("float32",[None,30,1])
+	Y = tf.placeholder("float32",[None,30,1])
+
+	# Input to hidden layer
+	cell = None
+	h = None
+	#h_b = None
+
+	cell = BasicLSTMCell(n_hidden, state_is_tuple=True, forget_bias=1)
+	if h == None:
+		h = cell.zero_state(1,tf.int32)
+	hidden_out, states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+
+
+	# Hidden Layer to Output
+	V_init_val = np.sqrt(6.)/np.sqrt(n_output + n_input)
+
+	V_weights = tf.get_variable("V_weights", shape = [n_hidden, n_output], \
+			dtype=tf.float32, initializer=tf.random_uniform_initializer(-V_init_val, V_init_val))
+	V_bias = tf.get_variable("V_bias", shape=[n_output], \
+			dtype=tf.float32, initializer=tf.constant_initializer(0.01))
+
+	hidden_out_list = tf.unstack(hidden_out, axis=1)
+	temp_out = tf.stack([tf.matmul(i, V_weights) for i in hidden_out_list])
+	output_data = tf.nn.bias_add(tf.transpose(temp_out, [1,0,2]), V_bias)
+
+
+	# define evaluate process
+	print("Output data: " , output_data)
+	print("Labels: " , Y)
+	cost = tf.reduce_sum(tf.square(output_data-Y))
+	correct_pred = tf.equal(tf.round(output_data*standardDev+meanVal), tf.round(Y*standardDev+meanVal))
+	accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+
+	# --- Initialization ----------------------
+	global_step = tf.Variable(0, trainable=False)
+
+	learning_rate = tf.train.exponential_decay(learning_rate, global_step,
+                                           100, 0.99, staircase=True)
+	optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate, decay=decay).minimize(cost)
+	init = tf.global_variables_initializer()
+
+	for i in tf.global_variables():
+		print(i.name)
+
+	step = 0
+	with tf.Session(config=tf.ConfigProto(log_device_placement=False, allow_soft_placement=False)) as sess:
+		print("Session Created")
+
+		steps = []
+		losses = []
+		accs = []
+		validation_losses = []
+		curEpoch = 0
+
+		sess.run(init)
+		training_state = None
+		i = -1
+		print ("Number train: " , len(data))
+		train_file_name = "loss.csv"
+		train_loss_file = open(train_file_name,'w')
+
+		while curEpoch < numEpochs:
+			i += 1
+			if ((i+1) > (len(data)-1.0)/30.0):
+				i = 0
+				curEpoch += 1
+			myTrain_x = data[30*i:30*(i+1),0:1].reshape((1,30,1))
+			myTrain_y = data[30*i+1:30*(i+1)+1,0:1].reshape((1,30,1))
+
+			myfeed_dict={X: myTrain_x, Y: myTrain_y}
+			if training_state is not None:
+				myfeed_dict[h] = training_state
+			
+			empty,acc,loss,training_state,output_data_2 = sess.run([optimizer, accuracy, cost, states,output_data], feed_dict = myfeed_dict)
+			#print("TrainX: ", myTrain_x)
+			
+			print("Iter " + str(i) + ", Minibatch Loss= " + \
+				  "{:.6f}".format(loss) + ", Training Accuracy= " + \
+			  	  "{:.5f}".format(acc))
+			if (i % 100 == 0):
+				outputVal = np.around(np.array(output_data_2*standardDev+meanVal))
+				correctVal = myTrain_y*standardDev+meanVal
+				print("Output: " , outputVal)
+				print("My train: " , correctVal)
+				print("Output - myTrain: " , outputVal-correctVal)
+				
+				train_loss_file.write(str(loss*standardDev)+"\n")
+				train_loss_file.flush()
+		train_loss_file.close()
+			
+
+			
+		print("Optimization Finished!")
+		
+
+
+if __name__=="__main__":
+	
+	main()
---
+++ plotValues.py
@@ -0,0 +1,24 @@
+#THis plots the values
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+#'results/Dielectric_Massive/train_train_loss_0.txt',
+loss_files=['loss.csv','loss_withVolume.csv']
+
+#Dielectric_Order_BiasTest/train_val_loss_1_val_bias_test.txt','results/Dielectric_Order_BiasTest/train_val_loss_val_bias_test_sigmoid_nobias.txt']
+
+lossValues = np.genfromtxt(loss_files[0],delimiter=',')
+newVals = range(0,len(lossValues))
+plt.plot(newVals,lossValues)
+
+lossValues2 = np.genfromtxt(loss_files[1],delimiter=',')
+newVals2 = range(0,len(lossValues2))
+plt.plot(newVals2,lossValues2)
+
+
+plt.xlabel("Steps (in 10's)")
+plt.ylabel("MSE Training Error - Normalized points")
+plt.title("Training Error error")
+plt.legend(['Without Volume', 'With Volume'])
+#plt.plot(lossValues_oldbatch)
+plt.show()
